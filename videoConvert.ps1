a9c093e110c9859d398137eda8d55143fdce9340
Marcelo Rocha <murchelon@users.noreply.github.com>
2025-05-26 02:31:49 -0300

    init commit

---
+++ videoConvert.ps1
@@ -0,0 +1,80 @@
+<#
+videoConvert
+#>
+
+param (
+    
+    # folder where the videos are or start folder to do a recursive convert
+    [string] $videoFolderToConvert = 'D:\GITREP\Murch\videoConvert\videos para converter\222',
+    
+    # types of videos to convert
+    [string[]] $convertFileTypes = @(
+        'mov',
+        'avi',
+        'mpg',
+        'mpeg',
+        'webm',
+        '3gp'
+    ),
+
+    # convert recursive? Convert all videos in any folder inside the start folder
+    [bool] $isRecursive = $false
+
+    )
+
+$convertFileTypes = $convertFileTypes | ForEach-Object { $_.ToLower() }
+
+$getChilderParams = @{
+    Path = $videoFolderToConvert
+    File = $true
+}
+
+if ($isRecursive)
+{
+    $getChilderParams.Recurse = $true
+}
+
+
+$files = Get-ChildItem @getChilderParams
+
+
+$files_toConvert = $files | Where-Object {
+    $file_ext = $_.Extension.TrimStart('.').ToLower()
+    $convertFileTypes -contains $file_ext
+}
+
+
+$files_toConvert | ForEach-Object {
+
+    $input = $_.FullName
+    $folder = $_.DirectoryName
+
+    $baseName = [System.IO.Path]::GetFileNameWithoutExtension($input)
+
+    # H.264
+    $output264 = Join-Path $folder "$baseName`__converted_h264.mp4"
+    if (-not (Test-Path $output264)) {
+        ffmpeg -i "$input" -c:v libx264 -crf 24 -preset slow -c:a aac -b:a 192k "$output264"
+    } else {
+        Write-Host "Pulando: $output264 já existe."
+    }  
+
+    # # H.265
+    # $output265 = Join-Path $folder "$baseName`__converted_h265.mp4"
+    # if (-not (Test-Path $output265)) {
+    #     ffmpeg -i "$input" -c:v libx265 -crf 24 -preset slow -c:a aac -b:a 192k "$output265"
+    # } else {
+    #     Write-Host "Pulando: $output265 já existe."
+    # }  
+
+    # # AV1 (SVT-AV1)
+    # $outputAV1 = Join-Path $folder "$baseName`__converted_av1.mkv"
+    # if (-not (Test-Path $outputAV1)) {
+    #     ffmpeg -i "$input" -c:v libsvtav1 -crf 22 -preset 4 -c:a aac -b:a 192k "$outputAV1"
+    # } else {
+    #     Write-Host "Pulando: $outputAV1 já existe."
+    # }  
+
+
+    
+}
